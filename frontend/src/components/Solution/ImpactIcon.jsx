7f95483bcd1f158c188aa2e048e9e8e5fab545eb
treinhard <thomas.reinhard@gmail.com>
2023-10-31 15:57:20 +0100

    Impact Icon completed incl. Styling. Not yet connected to backend. Styling needs to be revised once the SolutionDropdown component is done.

---
+++ frontend/src/components/Solution/ImpactIcon.jsx
@@ -0,0 +1,12 @@
+import {ImpactIconDiv} from "./solution.style.js";
+
+function ImpactIcon(){
+        let impact = -5
+
+    return(
+        <ImpactIconDiv>
+            <div>{impact}</div>
+        </ImpactIconDiv>
+    )
+}
+export default ImpactIcon
--- frontend/src/components/Solution/solution.style.js
+++ frontend/src/components/Solution/solution.style.js
@@ -12,6 +12,13 @@ export const NumberOfSupporters = styled.div``;
 
 export const CategoryLabel = styled.div``;
 
-export const ImpactIcon = styled.div``;
+export const ImpactIconDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  color: white;
+  border-radius: 10px;
+`;
 
 export const LevelButton = styled.div``;
