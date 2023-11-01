0d478b9949c9e1a1f1dbaa9c32c8eff2b0ef81b7
alona <alionazhv@gmail.com>
2023-11-01 16:15:26 +0100

    Initial Layout for FilterSearch is applied

--- frontend/src/components/SolutionFilter/SolutionFilter.jsx
+++ frontend/src/components/SolutionFilter/SolutionFilter.jsx
@@ -1,41 +1,70 @@
 import React, { useState } from 'react';
 import filter from '../../assets/other_icons/filter.svg';
-import { StyledImage } from './SolutionFilter.style.js';
+import filter_icon from '../../assets/images/Filter Icon(1).png';
+import sorting_icon from '../../assets/images/Sort Icon.png';
+import {
+  DropdownLayout,
+  StyledImage,
+  DropdownContent,
+  DropdownSelect,
+  DropdownSort, // Added for sorting section
+  SortingOption, TitleAndImage, // Added for sorting options
+} from './SolutionFilter.style.js';
 
 export default function SolutionFilter() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const handleSelectChange = (e) => {
+    setSelectedCategory(e.target.value);
+    setIsDropdownOpen(false);
+  };
+
   return (
-    <>
+    <DropdownLayout>
       <StyledImage src={filter} onClick={toggleDropdown} alt="Filter" />
       {isDropdownOpen && (
-        <div>
-          <label htmlFor="category">Category:</label>
-          <select id="category">
-            <option value="all">All</option>
-            <option value="buildings">Buildings</option>
-          </select>
-
-          <label htmlFor="impact">Impact:</label>
-          <select id="impact">
-            <option value="all">All</option>
-            <option value="low">Low</option>
-            {/* Add more impact options */}
-          </select>
-
-          <label htmlFor="status">Status:</label>
-          <select id="status">
-            <option value="all">All</option>
-            <option value="open">Open</option>
-            <option value="done">Done</option>
-          </select>
-          <button>Apply Filter</button>
-        </div>
+        <DropdownContent>
+          <TitleAndImage>
+          <img src={filter_icon} alt="Filter Icon" />
+            <h3>Category Filter</h3>
+          </TitleAndImage>
+          <DropdownSelect value={selectedCategory} onChange={handleSelectChange}>
+            <option value="">All</option>
+            <option value="Buildings">Buildings</option>
+            <option value="Electricity">Electricity</option>
+            <option value="Food">Food</option>
+            <option value="Import">Import</option>
+            <option value="Industry">Industry</option>
+            <option value="Innovation">Innovation</option>
+            <option value="Money">Money</option>
+            <option value="Nature">Nature</option>
+            <option value="Transport">Transport</option>
+            <option value="Trash">Trash</option>
+          </DropdownSelect>
+          <DropdownSort>
+            <TitleAndImage>
+            <img src={sorting_icon} alt="Sort Icon" style={{"width": "20px"}}/>
+              <h3>Sorting Options</h3>
+              </TitleAndImage>
+            <SortingOption value="Impact">Impact</SortingOption>
+            <SortingOption value="Alphabetically">Alphabetically</SortingOption>
+            <SortingOption value="Number of Supporters">Number of Supporters</SortingOption>
+          </DropdownSort>
+          <DropdownSort>
+            <TitleAndImage>
+          <img src={filter_icon} alt="Filter Icon" />
+            <h3>Status Filter</h3>
+          </TitleAndImage>
+            <SortingOption value="Open">Open</SortingOption>
+            <SortingOption value="Done">Done</SortingOption>
+          </DropdownSort>
+        </DropdownContent>
       )}
-    </>
+    </DropdownLayout>
   );
 }
--- frontend/src/components/SolutionFilter/SolutionFilter.style.js
+++ frontend/src/components/SolutionFilter/SolutionFilter.style.js
@@ -1,19 +1,61 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const StyledImage = styled.img `
+export const StyledImage = styled.img`
   width: 40px;
-  height: 40px;`
+  height: 40px;
+`;
 
-export const FilterSection = styled.div `
-    display: flex;
+export const DropdownLayout = styled.div`
+  position: relative;
+`;
+
+export const DropdownContent = styled.div`
+  position: absolute;
+  display: flex;
   flex-direction: column;
-`
+  align-items: flex-start;
+  top: 100%;
+  left: 0;
+  width: 21rem; 
+  background-color: ${(props) => props.theme.backgroundColors.secondary};
+  border: 1px solid ${(props) => props.theme.backgroundColors.impactIconSelected};
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+  z-index: 1;
+  padding: 10px;
+  border-radius: 5px;
+  gap: 1.4rem;
+  height: 27rem;
+`;
 
-export const FilterLabel = styled.div `
-`
+export const TitleAndImage = styled.div `
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  margin-bottom: 0.5rem;`
 
-export const FilterInput = styled.div `
-`
+export const DropdownSelect = styled.select`
+  width: 100%;
+  padding: 5px;
+  margin-top: 0px;
+  border: 1px solid ${(props) => props.theme.backgroundColors.lightGrayBorder};
+  border-radius: 5px;
+  background-color: white;
+`;
 
-export const FilterPanel = styled.div `
-`
+export const DropdownSort = styled.div`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SortingOption = styled.option`
+  padding: 5px;
+  background-color: white;
+`;
+
+export const SortingImage = styled.image `
+  width: 35px;
+  height: 35px`
+
+DropdownSort.displayName = 'DropdownSort';
+SortingOption.displayName = 'SortingOption';
