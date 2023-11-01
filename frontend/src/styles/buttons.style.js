fa273469ff8fcbb09205dc2fe370df98a4c92f69
alona <alionazhv@gmail.com>
2023-11-01 12:19:00 +0100

    Adjustments to the header styling

---
+++ frontend/src/styles/buttons.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const buttonsStyle = styled.button`
+    background-color: black;
+    color: white;
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.25);
+    padding: 20px 40px 20px 40px;
+`
