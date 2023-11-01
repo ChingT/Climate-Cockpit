d4676ccb43c1a96da48752667532af84837398c8
alona <alionazhv@gmail.com>
2023-11-01 08:37:14 +0100

    Initial styling for hero and solution pages

--- frontend/src/pages/HeroPage.jsx
+++ frontend/src/pages/HeroPage.jsx
@@ -1,12 +1,15 @@
 import Hero3DCloud from "../components/HeroPageComponents/Hero3DCloud.jsx";
 import HeroPageButton from "../components/HeroPageComponents/HeroPageButton.jsx";
+import styled from "styled-components";
 
+const Main =styled.div `
+    margin-top: 4rem`
 const HeroPage = () => {
   return (
-    <>
+    <Main>
       <Hero3DCloud />
       <HeroPageButton />
-    </>
+    </Main>
   );
 };
 
--- frontend/src/pages/SolutionsPage.jsx
+++ frontend/src/pages/SolutionsPage.jsx
@@ -2,15 +2,18 @@ import TotalPoints from "../components/Dashboard/TotalPoints.jsx";
 import DashboardCategoriesGrid from "../components/Dashboard/DashboardCategoriesGrid.jsx";
 import SolutionDropDown from "../components/Solution/SolutionDropDown.jsx";
 import SolutionFilter from "../components/SolutionFilter/SolutionFilter.jsx";
+import styled from "styled-components";
 
+const Main = styled.div `
+    margin-top: 4rem`
 const SolutionsPage = () => {
   return (
-    <>
+    <Main>
       <TotalPoints />
       <DashboardCategoriesGrid />
       <SolutionFilter />
       <SolutionDropDown />
-    </>
+    </Main>
   );
 };
 
