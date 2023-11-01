fbaec151f8eade2c2f7d76de419b4b84d11f02ac
alona <alionazhv@gmail.com>
2023-11-01 14:55:30 +0100

    Prettier is applied

--- frontend/src/components/HeroPageComponents/HeroPageButton.jsx
+++ frontend/src/components/HeroPageComponents/HeroPageButton.jsx
@@ -1,8 +1,4 @@
-import {ButtonsStyle} from "../../styles/buttons.style.js"
+import { ButtonsStyle } from "../../styles/buttons.style.js";
 export default function HeroPageButton() {
-  return (
-      <ButtonsStyle>
-        Start Your Climate Journey
-      </ButtonsStyle>
-)
+  return <ButtonsStyle>Start Your Climate Journey</ButtonsStyle>;
 }
--- frontend/src/styles/buttons.style.js
+++ frontend/src/styles/buttons.style.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const ButtonsStyle = styled.button`
-    background-color: ${(props) => props.theme.colors.primary};
-    color: ${(props) => props.theme.fontColors.button};
-    border-radius: 10px;
-    box-shadow: rgba(0, 0, 0, 0.25);
-    padding: 20px 40px 20px 40px;
+  background-color: ${(props) => props.theme.colors.primary};
+  color: ${(props) => props.theme.fontColors.button};
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.25);
+  padding: 20px 40px 20px 40px;
 `;
