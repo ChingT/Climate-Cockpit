cd8ce4eaa61cfa1b402952720cc948a802a63ef5
treinhard <thomas.reinhard@gmail.com>
2023-11-01 09:44:11 +0100

    changed style name to impactIcon

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -9,13 +9,13 @@ const theme = {
   backgroundColors: {
     primary: "rgba(253, 247, 221, 0.2)",
     secondary: "white",
-    iconUnselected: "black",
-    iconSelected: "blue",
+    impactIconUnselected: "black",
+    impactIconSelected: "blue",
   },
   fontColors: {
     primary: "black",
     secondary: "#4c4c4c",
-    iconFont: "white",
+    impactIcon: "white",
   },
   fontFamily: {
     fontFamily: "Roboto",
