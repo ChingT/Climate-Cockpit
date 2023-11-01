37c7fc7cdf26df4f24eff8d630309c49664052c6
alona <alionazhv@gmail.com>
2023-11-01 08:18:51 +0100

    Adjustments to the MainLayout are made

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/header_icons/logo.png";
 import avatarImage from "../../assets/svgs/avatar.svg";
-import findFriendLogo from "../../assets/svgs/icon-friends.svg";
 import MenuDot from "../../assets/svgs/menu.svg";
-import bell from "../../assets/svgs/notification_bell.svg";
-import postsLogo from "../../assets/svgs/posts_logo.svg";
+import findFriendLogo from "../../assets/header_icons/find_friends.png";
+import lightbulb from "../../assets/header_icons/lightbulb-6624237-5487997.png";
+import posts from "../../assets/header_icons/posts.png";
+import posts_text from "../../assets/header_icons/posts_text.png";
+import find_friends_text from "../../assets/header_icons/find_friends_text.png";
+import solutions from "../../assets/header_icons/solutions.png";
+import profile from "../../assets/header_icons/profile.png";
+import social from "../../assets/header_icons/social.png";
 import useAutoFetch from "../../hooks/useAutoFetch.js";
 import { setRequests } from "../../store/slices/friendRequests.js";
 import FriendsRequestsContainer from "./FriendsRequests/FriendsRequestsContainer.jsx";
@@ -15,11 +20,17 @@ import {
   ContainerLeft,
   ContainerRight,
   HeaderContainer,
-  LogoWrapper,
-  MenuContainer,
+  LogoWrapper, MenuContainer,
   NavbarLink,
   NotificationButton,
+  PostsTextImage,
+  ProfileWrapper,
+  SocialWrapper,
+  SolutionWrapper,
+  StyledImage,
+  TextImage,
 } from "./Navigation.style.js";
+import navigationActionsContainer from "./NavigationActionsContainer.jsx";
 import NavigationActionsContainer from "./NavigationActionsContainer.jsx";
 
 const Navigation = () => {
@@ -60,28 +71,45 @@ const Navigation = () => {
       <ContainerLeft>
         <LogoWrapper to="/">
           <img src={logo}></img>
-          <p>Motion</p>
         </LogoWrapper>
+        <SolutionWrapper to="/solutions">
+          <img src={solutions}></img>
+        </SolutionWrapper>
+        <ProfileWrapper to="/profile">
+          <img src={profile}></img>
+        </ProfileWrapper>
+        <SocialWrapper to="/posts">
+          <img src={social}></img>
+        </SocialWrapper>
 
-        <nav>
-          <NavbarLink to="/posts">
-            <img src={postsLogo}></img>
-            <p>Posts</p>
-          </NavbarLink>
-          <NavbarLink to="/find-friends">
-            <img src={findFriendLogo}></img>
-            <p>Find Friends</p>
-          </NavbarLink>
+         <nav>
+          {loggedInUser ? (
+            <>
+              <NavbarLink to="/posts">
+                <StyledImage src={posts} alt="Image description" />
+                <PostsTextImage src={posts_text} alt="Posts Text" />
+              </NavbarLink>
+              <NavbarLink to="/find-friends">
+                <StyledImage src={findFriendLogo} alt="Find Friends" />
+                <TextImage src={find_friends_text} alt="Find Friends Text" />
+              </NavbarLink>
+            </>
+          ) : null}
         </nav>
       </ContainerLeft>
 
+      <nav>
+          {loggedInUser ? (
+            <>
       <ContainerRight>
         <NotificationButton
           onClick={() => setShowNotifications(!showNotifications)}
         >
           <div className={"icon-wrapper"}>
-            <img src={bell}></img>
-            <p className={"request-count"}>{requestsCount}</p>
+            <img src={lightbulb} alt="Lightbulb" />
+            {requestsCount >= 0 && (
+              <p className={"request-count"}>{requestsCount}</p>
+            )}
           </div>
           {showNotifications && (
             <FriendsRequestsContainer
@@ -92,13 +120,22 @@ const Navigation = () => {
           )}
         </NotificationButton>
         <Link to={"/profile"}>
-          <Avatar src={loggedInUser?.avatar || avatarImage}></Avatar>
+          <Avatar src={loggedInUser?.avatar || avatarImage} alt="Avatar" />
         </Link>
         <MenuContainer>
-          <img src={MenuDot} onClick={() => setShowMenu(!showMenu)} />
-          {showMenu && <NavigationActionsContainer setShowMenu={setShowMenu} />}
+          <img
+            src={MenuDot}
+            alt="Menu"
+            onClick={() => setShowMenu(!showMenu)}
+          />
+          {showMenu && (
+            <NavigationActionsContainer setShowMenu={setShowMenu} />
+          )}
         </MenuContainer>
       </ContainerRight>
+              </>
+          ) : null}
+        </nav>
     </HeaderContainer>
   );
 };
