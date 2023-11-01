c71ca87affd1fa96cd81e6db0ecd3b66c7ad16cb
ChingT <j60626j@gmail.com>
2023-11-01 10:58:01 +0100

    Fix using props in background-color

--- frontend/src/components/Solution/solution.style.js
+++ frontend/src/components/Solution/solution.style.js
@@ -16,8 +16,11 @@ export const ImpactIconDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${props => (isSelected ? props.theme.backgroundColors.iconSelected : props.theme.backgroundColors.iconUnselected)};
-  color: ${(props => props.theme.fontColors.impactIcon});;
+  background-color: ${(props) =>
+    props.isSelected
+      ? props.theme.backgroundColors.impactIconSelected
+      : props.theme.backgroundColors.impactIconUnselected};
+  color: ${(props) => props.theme.fontColors.impactIcon};
   border-radius: 10px;
   width: 6vh;
   height: 6vh;
