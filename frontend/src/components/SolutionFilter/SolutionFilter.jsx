b70ba8b049b451c99bb0fbbfa8d87e73abe21033
alona <alionazhv@gmail.com>
2023-11-01 14:54:43 +0100

    filter icon is configured

--- frontend/src/components/SolutionFilter/SolutionFilter.jsx
+++ frontend/src/components/SolutionFilter/SolutionFilter.jsx
@@ -1,10 +1,41 @@
+import React, { useState } from 'react';
+import filter from '../../assets/other_icons/filter.svg';
+import { StyledImage } from './SolutionFilter.style.js';
+
 export default function SolutionFilter() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <>
-      <>X</>
-      <>Category Filter</>
-      <>Sort</>
-      <>Status Filter</>
+      <StyledImage src={filter} onClick={toggleDropdown} alt="Filter" />
+      {isDropdownOpen && (
+        <div>
+          <label htmlFor="category">Category:</label>
+          <select id="category">
+            <option value="all">All</option>
+            <option value="buildings">Buildings</option>
+          </select>
+
+          <label htmlFor="impact">Impact:</label>
+          <select id="impact">
+            <option value="all">All</option>
+            <option value="low">Low</option>
+            {/* Add more impact options */}
+          </select>
+
+          <label htmlFor="status">Status:</label>
+          <select id="status">
+            <option value="all">All</option>
+            <option value="open">Open</option>
+            <option value="done">Done</option>
+          </select>
+          <button>Apply Filter</button>
+        </div>
+      )}
     </>
   );
 }
---
+++ frontend/src/components/SolutionFilter/SolutionFilter.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledImage = styled.img `
+  width: 40px;
+  height: 40px;`
+
+export const FilterSection = styled.div `
+    display: flex;
+  flex-direction: column;
+`
+
+export const FilterLabel = styled.div `
+`
+
+export const FilterInput = styled.div `
+`
+
+export const FilterPanel = styled.div `
+`
