92df919218b373aba4a96e499c129a1eec915339
alona <alionazhv@gmail.com>
2023-11-02 15:21:32 +0100

    ScorecardCategory is configured

--- frontend/src/components/ScoreCard/ScorecardCategory.jsx
+++ frontend/src/components/ScoreCard/ScorecardCategory.jsx
@@ -1,12 +1,54 @@
-import { CategoryLabel } from "../Solution/solution.style.js";
+import React from "react";
+import {
+    CategoryBar,
+    CategoryPart,
+    LevelNames,
+    ScorecardContainer, TitleAndBar,
+} from "./Scorecard.style.js";
+import {CategoryLabelDiv} from "../Solution/solution.style.js";
+import Score from "./Score.jsx";
 
-export default function ScoreCardCategory() {
+function ScorecardCategory() {
+  const data = [
+    {
+      categoryName: "Electricity",
+      levelNames: {
+        1: "Clean",
+        2: "Renewable",
+        3: "Sun Energy",
+      },
+      categoryLevel: 2,
+    },
+  ];
+
   return (
-    <>
-      <CategoryLabel />
-      <>Level</>
-      <>Level Tags</>
-      <>Sum</>
-    </>
+    <ScorecardContainer>
+      {data.map((category, index) => (
+        <div key={index}>
+            <TitleAndBar>
+                <CategoryLabelDiv>{category.categoryName}</CategoryLabelDiv>
+          <CategoryBar>
+            {Array.from({ length: 3 }).map((_, partIndex) => (
+              <CategoryPart
+                key={partIndex}
+                isFilled={partIndex < category.categoryLevel}
+              />
+            ))}
+          </CategoryBar>
+                </TitleAndBar>
+          <LevelNames>
+            {Object.keys(category.levelNames).map((level) => (
+              <span key={level}>
+                {category.levelNames[level]}
+                {parseInt(level) !== Object.keys(category.levelNames).length}
+              </span>
+            ))}
+          </LevelNames>
+        </div>
+      ))}
+        <Score />
+    </ScorecardContainer>
   );
 }
+
+export default ScorecardCategory;
