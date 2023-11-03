612ac2075b0c2fbb498c4d357e3ed22c4e555933
alona <alionazhv@gmail.com>
2023-11-03 11:34:07 +0100

    Styling for CircleShapedEmissionPoints element is configured

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -20,6 +20,12 @@ const theme = {
     impactIconUnselected: "black",
     impactIconSelected: "#0077BF",
   },
+  emissionColors: {
+    imported: "red",
+    inland: "red",
+    removed: "green",
+    solution: "blue",
+  },
   categoryLabels: {
     default: "white",
     electricity: "purple",
@@ -39,6 +45,7 @@ const theme = {
     primary: "black",
     secondary: "#4c4c4c",
     impactIcon: "white",
+    emissionLevel: "white",
     button: "white",
     emissionColor: "red",
     solutionColor: "blue",
