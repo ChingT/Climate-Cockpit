07906f2b2b60890849ce64afc475a1189f425771
ChingT <j60626j@gmail.com>
2023-11-06 12:31:37 +0100

    Use TimeStampedModel as base model for Comment

--- backend/app/comment/models.py
+++ backend/app/comment/models.py
@@ -1,15 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 from post.models import Post
 
 User = get_user_model()
 
 
-class Comment(models.Model):
+class Comment(TimeStampedModel):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="comments")
-    created = models.DateTimeField(auto_now_add=True)
-    comment = models.TextField()
+    content = models.TextField()
 
     def __str__(self):
         max_length = 30
