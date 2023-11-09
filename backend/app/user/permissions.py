495c6c5a829ceacd9d2b8c528e31fdefb29d36e7
ChingT <j60626j@gmail.com>
2023-11-09 16:27:40 +0100

    Fix permissions

--- backend/app/user/permissions.py
+++ backend/app/user/permissions.py
@@ -1,19 +1,4 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
-
-
-class IsAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_staff
-
-
-class ReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
-
-
-class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS or request.user and request.user.is_staff
+from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
