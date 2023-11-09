3e91c47292babc4fc67bc8683c9bb3ca66bafee3
ChingT <j60626j@gmail.com>
2023-11-09 11:38:11 +0100

    Rename StyledLoader as LoadingSpinner

--- frontend/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -2,24 +2,24 @@ import styled, { keyframes } from "styled-components";
 
 const animloader = keyframes`
   0% {
-    box-shadow: 25px 0 white, 50px 0 white, 75px 0 white, 100px 0 white, 125px 0 white, 
-    150px 0 white, 25px 0 white, 50px 0 white, 75px 0 white, 100px 0 white, 125px 0 white, 
+    box-shadow: 25px 0 white, 50px 0 white, 75px 0 white, 100px 0 white, 125px 0 white,
+    150px 0 white, 25px 0 white, 50px 0 white, 75px 0 white, 100px 0 white, 125px 0 white,
     150px 0 white;
   }
   50% {
-    box-shadow: 25px 20px rgba(255, 255, 255, 0.8), 50px 60px rgba(255, 255, 255, 0.8), 
-    75px 30px rgba(255, 255, 255, 0.8), 100px 70px rgba(255, 255, 255, 0.8), 125px 40px 
-    rgba(255, 255, 255, 0.8), 150px 60px rgba(255, 255, 255, 0.8), 25px 20px rgba(255, 
-    255, 255, 0.8), 50px 30px rgba(255, 255, 255, 0.8), 75px 10px rgba(255, 255, 255, 
-    0.8), 100px 30px rgba(255, 255, 255, 0.8), 125px 30px rgba(255, 255, 255, 0.8), 
+    box-shadow: 25px 20px rgba(255, 255, 255, 0.8), 50px 60px rgba(255, 255, 255, 0.8),
+    75px 30px rgba(255, 255, 255, 0.8), 100px 70px rgba(255, 255, 255, 0.8), 125px 40px
+    rgba(255, 255, 255, 0.8), 150px 60px rgba(255, 255, 255, 0.8), 25px 20px rgba(255,
+    255, 255, 0.8), 50px 30px rgba(255, 255, 255, 0.8), 75px 10px rgba(255, 255, 255,
+    0.8), 100px 30px rgba(255, 255, 255, 0.8), 125px 30px rgba(255, 255, 255, 0.8),
     150px 30px rgba(255, 255, 255, 0.8);
   }
   100% {
-    box-shadow: 25px 60px rgba(255, 255, 255, 0.8), 50px 60px rgba(255, 255, 255, 0.8), 
-    75px 50px rgba(255, 255, 255, 0.8), 100px 70px rgba(255, 255, 255, 0.8), 125px 70px 
-    rgba(255, 255, 255, 0.8), 150px 60px rgba(255, 255, 255, 0.8), 25px 80px 
-    rgba(255, 255, 255, 0.8), 50px 80px rgba(255, 255, 255, 0.8), 75px 70px 
-    rgba(255, 255, 255, 0.8), 100px 60px rgba(255, 255, 255, 0.8), 125px 30px 
+    box-shadow: 25px 60px rgba(255, 255, 255, 0.8), 50px 60px rgba(255, 255, 255, 0.8),
+    75px 50px rgba(255, 255, 255, 0.8), 100px 70px rgba(255, 255, 255, 0.8), 125px 70px
+    rgba(255, 255, 255, 0.8), 150px 60px rgba(255, 255, 255, 0.8), 25px 80px
+    rgba(255, 255, 255, 0.8), 50px 80px rgba(255, 255, 255, 0.8), 75px 70px
+    rgba(255, 255, 255, 0.8), 100px 60px rgba(255, 255, 255, 0.8), 125px 30px
     rgba(255, 255, 255, 0.8), 150px 30px rgba(255, 255, 255, 0.8);
   }
 `;
@@ -67,8 +67,6 @@ const LoaderContainer = styled.div`
   }
 `;
 
-const StyledLoader = () => {
+export default function LoadingSpinner() {
   return <LoaderContainer className="loader" />;
-};
-
-export default StyledLoader;
+}
