30526070aa4679fc96d07952a0e7de953ade2c0c
ChingT <j60626j@gmail.com>
2023-11-10 11:36:23 +0100

    Load dummy resource data for news, books and papers

--- backend/app/fixtures/load_data.py
+++ backend/app/fixtures/load_data.py
@@ -26,14 +26,16 @@ def populate_resources(apps: AppConfig, schema_editor):
     Solution = apps.get_model("solution", "Solution")  # noqa: N806
     Resource = apps.get_model("solution", "Resource")  # noqa: N806
 
-    csv_file_path = Path("fixtures/source/solutions_videos.csv")
-    with Path.open(csv_file_path, encoding="utf-8") as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            data = dict(**row)
-            data["solution"] = get_object_or_404(Solution, name=data["solution"])
-            for field_name in row:
-                if not data[field_name]:
-                    del data[field_name]
+    resources_files = ["videos.csv", "news.csv", "books.csv", "papers.csv"]
+    for resources_file in resources_files:
+        csv_file_path = Path("fixtures/source/resource") / resources_file
+        with Path.open(csv_file_path, encoding="utf-8") as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                data = dict(**row)
+                data["solution"] = get_object_or_404(Solution, name=data["solution"])
+                for field_name in row:
+                    if not data[field_name]:
+                        del data[field_name]
 
-            Resource.objects.create(**data)
+                Resource.objects.create(**data)
