da420cf7013c406f6f62f902db494d6a5c6c55b9
ChingT <j60626j@gmail.com>
2023-11-13 20:07:06 +0100

    populate dashboard items

--- backend/app/solution/management/commands/populate_database.py
+++ backend/app/solution/management/commands/populate_database.py
@@ -4,7 +4,12 @@ from pathlib import Path
 
 from django.core.management.base import BaseCommand
 from rest_framework.generics import get_object_or_404
-from solution.solution_logic.models import SelectionRule, SolutionLogic
+from solution.solution_logic.models import (
+    DashboardGroup,
+    DashboardItem,
+    SelectionRule,
+    SolutionLogic,
+)
 from solution.solutions.models import Category, Resource, Solution
 
 source_root = Path("solution/management/commands/source")
@@ -18,12 +23,13 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS("Successfully imported solution data"))
 
         populate_resources()
-        self.stdout.write(self.style.SUCCESS("Successfully imported resource data"))
+        self.stdout.write(self.style.SUCCESS("Successfully imported resources"))
+
+        populate_dashboard_items()
+        self.stdout.write(self.style.SUCCESS("Successfully imported dashboard_items"))
 
         populate_solution_logics()
-        self.stdout.write(
-            self.style.SUCCESS("Successfully imported solution_logics data")
-        )
+        self.stdout.write(self.style.SUCCESS("Successfully imported solution_logics"))
 
 
 def populate_solutions():
@@ -35,7 +41,7 @@ def populate_solutions():
             data["category"], _ = Category.objects.get_or_create(name=data["category"])
 
             for field_name in row:
-                if not data[field_name]:
+                if data[field_name] is None or data[field_name] == "":
                     del data[field_name]
 
             Solution.objects.create(**data)
@@ -50,12 +56,31 @@ def populate_resources():
             for row in csv_reader:
                 data = dict(**row)
                 for field_name in row:
-                    if not data[field_name]:
+                    if data[field_name] is None:
                         del data[field_name]
 
                 data["solution"] = get_object_or_404(Solution, name=data["solution"])
                 data["resource_type"] = resources_type
                 Resource.objects.create(**data)
+
+
+def populate_dashboard_items():
+    fields = [field.name for field in DashboardItem._meta.get_fields()]  # noqa: SLF001
+
+    csv_file_path = source_root / "dashboard_item.csv"
+    with Path.open(csv_file_path, encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data = dict(**row)
+
+            data["group"], _ = DashboardGroup.objects.get_or_create(
+                name=data["group"], subgroup_name=data["subgroup"]
+            )
+
+            for field_name in row:
+                if data[field_name] is None or field_name not in fields:
+                    del data[field_name]
+            DashboardItem.objects.create(**data)
 
 
 def populate_solution_logics():
