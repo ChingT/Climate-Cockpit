687b97660e4327341c150deff5f4035eb9fb2724
alona <alionazhv@gmail.com>
2023-11-15 20:05:22 +0100

    Prettier got applied

--- frontend/src/components/FriendsGrid/FriendCard.jsx
+++ frontend/src/components/FriendsGrid/FriendCard.jsx
@@ -14,19 +14,21 @@ import {
   FriendLocation,
   FriendName,
   Memberships,
-  NameLocation, PopUp,
+  NameLocation,
+  PopUp,
   ReadMoreButton,
   RightBar,
   RightBottom,
   RightTop,
 } from "./FriendCard.style.js";
 
-
 const ReadMorePopup = ({ aboutMeText, onClose }) => {
   return (
     <PopUp>
       <p>{aboutMeText}</p>
-      <ReadMoreButton style = {{marginTop: "0.5rem"}} onClick={onClose}>close</ReadMoreButton>
+      <ReadMoreButton style={{ marginTop: "0.5rem" }} onClick={onClose}>
+        close
+      </ReadMoreButton>
     </PopUp>
   );
 };
@@ -49,69 +51,69 @@ const FriendCard = ({ friendInfo, requestObject }) => {
     : friendInfo.about_me.slice(0, MAX_CHARACTERS) +
       (friendInfo.about_me.length > MAX_CHARACTERS ? "..." : "");
 
- return (
-  <FriendCardContainer>
-    <FriendCardHeader to={`/profile/${friendInfo.id}`}>
-      <FriendAvatar alt="avatar" src={friendInfo.avatar || avatarImage} />
-    </FriendCardHeader>
-    <RightBar>
-      <RightTop>
-        <NameLocation>
-          <FriendName>
-            {friendInfo.first_name
-              ? `${friendInfo.first_name} ${friendInfo.last_name}`
-              : friendInfo.email
-              ? friendInfo.email.split("@")[0]
-              : ""}
-          </FriendName>
-          <FriendLocation>{friendInfo.location}</FriendLocation>
-        </NameLocation>
-        <ButtonsWrapper>
-          <FollowAddButtons
-            style={{ fontSize: "10px" }}
-            friendInfo={friendInfo}
-            requestObject={requestObject}
-          />
-        </ButtonsWrapper>
-      </RightTop>
-      <RightBottom>
-        <Memberships>
-          <LikedThingsContainer $centered>
-            <Grid>
-              {hasMemberships &&
-                friendInfo.memberships.map((thing) => (
-                  <li key={thing}>{thing}</li>
-                ))}
-            </Grid>
-          </LikedThingsContainer>
-        </Memberships>
-        <FriendAbout
-          fontSize={
-            friendInfo.about_me.length < 15
-              ? `${MIN_FONT_SIZE}px`
-              : hasMemberships
-              ? "1rem"
-              : "1.5rem"
-          }
-        >
-          {aboutMeText}
-          {friendInfo.about_me.length > MAX_CHARACTERS && (
-            <ReadMoreButton onClick={handleReadMoreClick}>
-              {showFullAbout ? "read less" : "read more"}
-            </ReadMoreButton>
-          )}
-
-          {isPopupOpen && (
-            <ReadMorePopup
-              aboutMeText={friendInfo.about_me}
-              onClose={handleReadMoreClick}
+  return (
+    <FriendCardContainer>
+      <FriendCardHeader to={`/profile/${friendInfo.id}`}>
+        <FriendAvatar alt="avatar" src={friendInfo.avatar || avatarImage} />
+      </FriendCardHeader>
+      <RightBar>
+        <RightTop>
+          <NameLocation>
+            <FriendName>
+              {friendInfo.first_name
+                ? `${friendInfo.first_name} ${friendInfo.last_name}`
+                : friendInfo.email
+                ? friendInfo.email.split("@")[0]
+                : ""}
+            </FriendName>
+            <FriendLocation>{friendInfo.location}</FriendLocation>
+          </NameLocation>
+          <ButtonsWrapper>
+            <FollowAddButtons
+              style={{ fontSize: "10px" }}
+              friendInfo={friendInfo}
+              requestObject={requestObject}
             />
-          )}
-        </FriendAbout>
-      </RightBottom>
-    </RightBar>
-  </FriendCardContainer>
-);
+          </ButtonsWrapper>
+        </RightTop>
+        <RightBottom>
+          <Memberships>
+            <LikedThingsContainer $centered>
+              <Grid>
+                {hasMemberships &&
+                  friendInfo.memberships.map((thing) => (
+                    <li key={thing}>{thing}</li>
+                  ))}
+              </Grid>
+            </LikedThingsContainer>
+          </Memberships>
+          <FriendAbout
+            fontSize={
+              friendInfo.about_me.length < 15
+                ? `${MIN_FONT_SIZE}px`
+                : hasMemberships
+                ? "1rem"
+                : "1.5rem"
+            }
+          >
+            {aboutMeText}
+            {friendInfo.about_me.length > MAX_CHARACTERS && (
+              <ReadMoreButton onClick={handleReadMoreClick}>
+                {showFullAbout ? "read less" : "read more"}
+              </ReadMoreButton>
+            )}
+
+            {isPopupOpen && (
+              <ReadMorePopup
+                aboutMeText={friendInfo.about_me}
+                onClose={handleReadMoreClick}
+              />
+            )}
+          </FriendAbout>
+        </RightBottom>
+      </RightBar>
+    </FriendCardContainer>
+  );
 };
 
 export default FriendCard;
