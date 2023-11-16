657b61fdb7b96f243bcd5957ed19ecdf65da0baa
ChingT <j60626j@gmail.com>
2023-11-16 12:18:30 +0100

    Rename chatbot as gtpbot

--- backend/app/post/signals.py
+++ backend/app/post/signals.py
@@ -18,25 +18,25 @@ User = get_user_model()
 
 
 @receiver(post_save, sender=Post)
-def create_gtpbot_comment_for_post(sender, instance, created, **kwargs):
+def create_gptbot_comment_for_post(sender, instance, created, **kwargs):
     if created:
-        create_gtpbot_comment(instance)
+        create_gptbot_comment(instance)
 
 
-def create_gtpbot_comment(target_post: Post):
-    gtpbot_users = User.objects.filter(is_GPTbot=True)
-    for gtpbot_user in gtpbot_users.filter(followers__id=target_post.user.id):
-        gtpbot_response = get_gtpbot_response(gtpbot_user, target_post.content)
-        create_comment(gtpbot_user, target_post, gtpbot_response)
+def create_gptbot_comment(target_post: Post):
+    gptbot_users = User.objects.filter(is_gptbot=True)
+    for gptbot_user in gptbot_users.filter(followers__id=target_post.user.id):
+        gptbot_response = get_gptbot_response(gptbot_user, target_post.content)
+        create_comment(gptbot_user, target_post, gptbot_response)
 
 
 def create_comment(user, post, content):
     Comment.objects.create(user=user, post=post, content=content)
 
 
-def get_gtpbot_response(gtpbot_user: User, user_text: str):
+def get_gptbot_response(gptbot_user: User, user_text: str):
     user_message = {"role": USER_ROLE, "content": user_text}
-    system_message = {"role": SYSTEM_ROLE, "content": gtpbot_user.GPTbot_description}
+    system_message = {"role": SYSTEM_ROLE, "content": gptbot_user.gptbot_description}
     messages_history = [system_message, user_message]
     response = client.chat.completions.create(
         model=OPENAI_MODEL_NAME, messages=messages_history
